758dfdb7bb5db02fe226d9725ea2e8749a8c3ee4
pilot <8565879+odcey@users.noreply.github.com>
2023-09-08 16:03:30 +0200

    chore: update nextjs & transpile method

--- packages/next-app/next.config.js
+++ packages/next-app/next.config.js
@@ -1,12 +1,11 @@
-const withTM = require('next-transpile-modules')(["@0xsquid/widget", "@0xsquid/staking-widget"]);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  transpilePackages: ["@0xsquid/widget", "@0xsquid/staking-widget"],
 }
 
 
 // Please declare withTM as your last plugin (the outermost one)
-module.exports = withTM(nextConfig)
+module.exports = nextConfig
 
