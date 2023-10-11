a569c39e1418a46c5e41ccbdd59100f725d90cc1
pilot <8565879+odcey@users.noreply.github.com>
2023-10-11 14:30:27 +0200

    chore: support v2 on cra-app

--- packages/cra-app/config-overrides.js
+++ packages/cra-app/config-overrides.js
@@ -1,10 +1,18 @@
-/* config-overrides.js */
-
 module.exports = function override(config, env) {
-    config.resolve.fallback = {
-        crypto: require.resolve('crypto-browserify'),
-        path: require.resolve('path-browserify'),
-        stream: require.resolve('stream-browserify'),
-    };
-    return config;
-}
+  // Update module rules
+  config.module.rules = config.module.rules.map(rule => {
+    if (rule.oneOf instanceof Array) {
+      rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/];
+    }
+    return rule;
+  });
+
+  // Set fallbacks
+  config.resolve.fallback = {
+    crypto: require.resolve('crypto-browserify'),
+    path: require.resolve('path-browserify'),
+    stream: require.resolve('stream-browserify'),
+  };
+  
+  return config;
+};
